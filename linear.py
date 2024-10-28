f568d642c35b19150e2b55d4a6f3e23c562f3ce3
hoangtuanlsht <151942543+hoangtuanlsht@users.noreply.github.com>
2024-10-29 03:28:53 +0700

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,7 +1,5 @@
-
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
